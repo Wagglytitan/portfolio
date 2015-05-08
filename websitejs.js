e326b2d13e2cacf6df57c8b768584ad7e1dcf119
Wagglytitan <lore.versmissen@live.be>
2015-05-08 12:08:07 +0000

    Mijn allereerste commit

---
+++ websitejs.js
@@ -0,0 +1,33 @@
+var main = function () {
+  "use strict";
+  $(".l1").on("click", function () {
+	  $('.s1').addClass('zichtbaar');
+	  $('.s2').removeClass('zichtbaar');
+	  $('.s3').removeClass('zichtbaar');
+  });
+  
+  $(".l2").on("click", function () {
+	 $('.s2').addClass('zichtbaar'); 
+	 $('.s1').removeClass('zichtbaar');
+	 $('.s3').removeClass('zichtbaar');
+  });
+  
+  $(".l3").on("click", function () {
+	  $('.s3').addClass('zichtbaar');
+	  $('.s1').removeClass('zichtbaar');
+	  $('.s2').removeClass('zichtbaar');
+  });
+  
+   $(".volgende1").on("click", function () {
+		 $(".ss1").removeClass("zichtbaar");
+	       $(".ss2").addClass("zichtbaar");
+	});
+	
+    $(".volgende2").on("click", function () {
+		 $(".ss2").removeClass("zichtbaar");
+	       $(".ss1").addClass("zichtbaar");
+	});	
+		 
+}
+
+$("document").ready(main);
