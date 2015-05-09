987b76064450a7bab187a77a7d5682b76f2830fb
Wagglytitan <lore.versmissen@live.be>
2015-05-09 13:31:47 +0000

    nieuwe versie

--- inspiratie/website2.js
+++ inspiratie/website2.js
@@ -1,6 +1,29 @@
 var main = function () {
   "use strict";
-  // hier komt je code
+    $(".doctor").on("mouseover", function () {
+    	$(".doctor1").css("display","block");
+		});
+		
+		$(".doctor").on("mouseout", function () {
+    	$(".doctor1").css("display","none");
+    });
+	
+	$(".nature").on("mouseover", function () {
+    	$(".nature1").css("display","block");
+    });
+	
+	$(".nature").on("mouseout", function () {
+    	$(".nature1").css("display","none");
+    });
+	
+	$(".space").on("mouseover", function () {
+    	$(".space1").css("display","block");
+    });
+	
+	$(".space").on("mouseout", function () {
+    	$(".space1").css("display","none");
+    });
+	
 }
 
 $("document").ready(main);
--- websitejs.js
+++ websitejs.js
@@ -4,16 +4,25 @@ var main = function () {
 	  $('.s1').addClass('zichtbaar');
 	  $('.s2').removeClass('zichtbaar');
 	  $('.s3').removeClass('zichtbaar');
+	  $('.s4').removeClass('zichtbaar');
   });
   
   $(".l2").on("click", function () {
 	 $('.s2').addClass('zichtbaar'); 
 	 $('.s1').removeClass('zichtbaar');
 	 $('.s3').removeClass('zichtbaar');
+	 $('.s4').removeClass('zichtbaar');
   });
   
   $(".l3").on("click", function () {
 	  $('.s3').addClass('zichtbaar');
+	  $('.s1').removeClass('zichtbaar');
+	  $('.s2').removeClass('zichtbaar');
+	  $('.s4').removeClass('zichtbaar');
+  });
+  
+   $(".l5").on("click", function () {
+	  $('.s4').addClass('zichtbaar');
 	  $('.s1').removeClass('zichtbaar');
 	  $('.s2').removeClass('zichtbaar');
   });
@@ -25,6 +34,11 @@ var main = function () {
 	
     $(".volgende2").on("click", function () {
 		 $(".ss2").removeClass("zichtbaar");
+	       $(".ss3").addClass("zichtbaar");
+	});	
+	
+	 $(".volgende3").on("click", function () {
+		 $(".ss3").removeClass("zichtbaar");
 	       $(".ss1").addClass("zichtbaar");
 	});	
 		 
